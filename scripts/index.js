09b457ab6af78d30b8ec9e5baa79b4dfc631b3ec
Leo Kraft <56594417+leokraft@users.noreply.github.com>
2021-11-28 08:03:05 +0100

    Another atempt to fix the waving on mobile

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,7 @@ $(".waving-emoji").bind("webkitAnimationEnd mozAnimationEnd animationend", funct
 $(".waving-emoji").hover(function(){
     $(this).addClass("waving-animation");        
 })
+
+$(".waving-emoji").click(function(){
+    $(this).addClass("waving-animation");        
+})
