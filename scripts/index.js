8c9bb9ba254bad808498ce6a252d5869742c7c64
Leo Kraft <56594417+leokraft@users.noreply.github.com>
2021-11-28 07:59:37 +0100

    Fix waving animation and add hello waving

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+$(".waving-emoji").bind("webkitAnimationEnd mozAnimationEnd animationend", function(){
+    $(this).removeClass("waving-animation hello-waving")
+})
+  
+$(".waving-emoji").hover(function(){
+    $(this).addClass("waving-animation");        
+})
